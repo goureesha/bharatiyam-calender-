1ad460f5cd0caacfbeab8115e76b6eb70d861742
goureesha <goureesha@users.noreply.github.com>
2026-08-12 05:22:34 +0000

    deploy: 5ec4fa3013a69bf6b8f5fa69319497ab07cd72a7

--- main.dart.js
+++ main.dart.js
@@ -31054,7 +31054,7 @@ $S:2}
 B.ayW.prototype={
 $1(a){var s=B.dQ().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/0cd610717bde95fd88343c64f81c11ba4e5c0010/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/5a2a6a42cce67f965cf540fcecf616faca624aa1/":s)+a},
 $S:141}
 B.LN.prototype={
 Zr(){var s=this.agL(),r=$.bm.aT().ImageFilter.MakeColorFilter(s,null)
