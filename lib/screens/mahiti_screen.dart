69dc8537204107448ce12dd8c5b207a1d0c325ae
goureesh <41700063+goureesha@users.noreply.github.com>
2026-09-03 16:29:06 +0530

    fix: add missing AppLocale import to mahiti_screen.dart

--- lib/screens/mahiti_screen.dart
+++ lib/screens/mahiti_screen.dart
@@ -2,6 +2,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import '../i18n/app_locale.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
