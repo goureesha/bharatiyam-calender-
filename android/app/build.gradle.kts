27928cbdd6fb79bb751e85befd33e6551c88a21b
goureesh <41700063+goureesha@users.noreply.github.com>
2026-09-10 10:15:45 +0530

    fix: use NDK r27 for 16KB page size compliance (Google Play requirement)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.bharatiyam.bharatiyam_panchanga"
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
