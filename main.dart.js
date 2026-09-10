225dd3d603878d8ebafdb2c1d846e1d8e119b4a6
goureesha <goureesha@users.noreply.github.com>
2026-09-10 04:48:18 +0000

    deploy: 27928cbdd6fb79bb751e85befd33e6551c88a21b

--- main.dart.js
+++ main.dart.js
@@ -36094,7 +36094,7 @@ $S:2}
 A.aNA.prototype={
 $1(a){var s=A.e7().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/a804b261645ef8c13eb3d5c44a5c2fb0340c5539/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/06a2e2a110089dff50fe635cffd2a61e1b24fbcd/":s)+a},
 $S:83}
 A.UH.prototype={
 gq(a){var s=this.a
